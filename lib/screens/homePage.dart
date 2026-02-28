96a0da2d419ad3c8024a0c5f33c8d9bf30e01a2c
Margarita <o.komarinets@gmail.com>
2026-02-28 22:46:04 +0500

    feat: add main NavigationBar with IndexedStack navigation

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:solo_learning/screens/mainScreen.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectIndex =0;
+  final List<Widget> _pages =[
+    mainScreen(),
+    Placeholder(),
+    Placeholder()
+  ];
+  @override
   Widget build(BuildContext context) {
     final backGroundColor = Theme.of(context).scaffoldBackgroundColor;
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
-      appBar: AppBar(
-
+      body: IndexedStack(
+        index: _selectIndex,
+        children: _pages,
       ),
+      bottomNavigationBar: NavigationBar(
+
+        selectedIndex: _selectIndex,
+        onDestinationSelected: (int index) => setState(() => _selectIndex= index),
+        indicatorColor: colorScheme.primary,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        backgroundColor: backGroundColor,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home_outlined ,color: Colors.white,), label: 'home'),
+          NavigationDestination(icon: Icon(Icons.task_outlined, color: Colors.white,), label: 'quest'),
+          NavigationDestination(icon: Icon(Icons.shopping_basket_outlined ,color: Colors.white,), label: 'shop')
+        ],
+      ),
+
 
     );
   }
