e7060405afaae740eed8cc74a552743e33453136
Margarita <kkom.margarita@gmail.com>
2026-05-30 22:02:00 +0500

    feat: add a timer display widget

---
+++ lib/presentation/features/focus_room/widgets/timer_display.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TimerDisplay extends StatelessWidget {
+  const TimerDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
